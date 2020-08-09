14750dce81b4ad0ad9c656ee5684c84d6b39219a
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-08-09 10:44:02 +0530

    SECRET_SESSION error came so fixed that

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(session({
-	secret: process.env.SESSION_SECRET,
+	secret: process.env.SECRET_SESSION,
 	resave: false,
 	saveUninitialized: false,
 	cookie: {
